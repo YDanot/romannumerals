821fc24d76e4474c4a2eeb966f84657558d43fbf
Yann DANOT <yann@arolla.fr>
2018-07-02 18:01:54 +0200

    add "never contains more than 4 times a letter except M" property test

--- src/test/java/fr/arolla/romannumerals/RomanNumeralsPBT.java
+++ src/test/java/fr/arolla/romannumerals/RomanNumeralsPBT.java
@@ -5,6 +5,7 @@ import com.pholser.junit.quickcheck.generator.InRange;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import org.hamcrest.CoreMatchers;
 import org.junit.Assume;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,23 +16,23 @@ public class RomanNumeralsPBT {
     private RomanNumerals romanNumerals = new IterativeRomanNumerals();
 
     @Property
-    public void should_only_contains_IVXLDCM(@InRange(minInt = 1, maxInt = 1000000) int numericValue) {
+    public void should_only_contains_IVXLDCM(@InRange(minInt = 1, maxInt = 10000) int numericValue) {
         assertThat(romanNumerals.toRoman(numericValue)).matches("[IVXLDCM]+");
     }
 
     @Property
-    public void should_never_contains_more_than_3_times_a_letter_except_M(@InRange(minInt = 1, maxInt = 1000000) int numericValue) {
+    public void should_never_contains_more_than_4_times_a_letter_except_M(@InRange(minInt = 1, maxInt = 10000) int numericValue) {
         String roman = romanNumerals.toRoman(numericValue);
-        assertThat(roman).doesNotMatch("I{4,}?");
-        assertThat(roman).doesNotMatch("V{4,}?");
-        assertThat(roman).doesNotMatch("X{4,}?");
-        assertThat(roman).doesNotMatch("L{4,}?");
-        assertThat(roman).doesNotMatch("D{4,}?");
-        assertThat(roman).doesNotMatch("C{4,}?");
+        assertThat(occurenceOfLetter(roman,'I')).isLessThanOrEqualTo(4);
+        assertThat(occurenceOfLetter(roman,'V')).isLessThanOrEqualTo(4);
+        assertThat(occurenceOfLetter(roman,'X')).isLessThanOrEqualTo(4);
+        assertThat(occurenceOfLetter(roman,'L')).isLessThanOrEqualTo(4);
+        assertThat(occurenceOfLetter(roman,'D')).isLessThanOrEqualTo(4);
+        assertThat(occurenceOfLetter(roman,'C')).isLessThanOrEqualTo(4);
     }
 
     @Property
-    public void M_should_always_be_first_Letter(@InRange(minInt = 1000, maxInt = 1000000) int numericValue) {
+    public void M_should_always_be_first_letter_when_number_greather_than_1000(@InRange(minInt = 1000, maxInt = 10000) int numericValue) {
         String roman = romanNumerals.toRoman(numericValue);
         Assume.assumeThat(roman, CoreMatchers.containsString("I"));
         Assume.assumeThat(roman, CoreMatchers.containsString("V"));
@@ -45,6 +46,10 @@ public class RomanNumeralsPBT {
         assertThat(roman.indexOf('L')).isGreaterThan(roman.lastIndexOf('M'));
         assertThat(roman.indexOf('D')).isGreaterThan(roman.lastIndexOf('M'));
         assertThat(roman.indexOf('C')).isGreaterThan(roman.lastIndexOf('M'));
+    }
+
+    private long occurenceOfLetter(String s, char c) {
+        return s.chars().filter(ch -> ch == c).count();
     }
 
 }
