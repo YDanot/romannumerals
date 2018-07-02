758a03a1024cebe159dbe49c550cf8d5841e1b2b
Yann DANOT <yann@arolla.fr>
2018-07-02 10:52:06 +0200

    All in one commit arghhhh !

---
+++ src/test/java/fr/arolla/romannumerals/RomanNumeralsPBT.java
@@ -0,0 +1,40 @@
+package fr.arolla.romannumerals;
+
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.InRange;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assume;
+import org.junit.runner.RunWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(JUnitQuickcheck.class)
+public class RomanNumeralsPBT {
+
+    private RomanNumerals romanNumerals = new RomanNumeralsWithMap();
+
+    @Property
+    public void should_only_contains_IVXLDCM(@InRange(minInt = 1, maxInt = 1000000) int numericValue) {
+        assertThat(romanNumerals.toRoman(numericValue)).matches("[IVXLDCM]+");
+    }
+
+    @Property
+    public void M_should_always_be_first_Letter(@InRange(minInt = 1000, maxInt = 1000000) int numericValue) {
+        String roman = romanNumerals.toRoman(numericValue);
+        Assume.assumeThat(roman, CoreMatchers.containsString("I"));
+        Assume.assumeThat(roman, CoreMatchers.containsString("V"));
+        Assume.assumeThat(roman, CoreMatchers.containsString("X"));
+        Assume.assumeThat(roman, CoreMatchers.containsString("C"));
+        Assume.assumeThat(roman, CoreMatchers.containsString("D"));
+        Assume.assumeThat(roman, CoreMatchers.containsString("L"));
+        assertThat(roman.indexOf('I')).isGreaterThan(roman.lastIndexOf('M'));
+        assertThat(roman.indexOf('V')).isGreaterThan(roman.lastIndexOf('M'));
+        assertThat(roman.indexOf('X')).isGreaterThan(roman.lastIndexOf('M'));
+        assertThat(roman.indexOf('L')).isGreaterThan(roman.lastIndexOf('M'));
+        assertThat(roman.indexOf('D')).isGreaterThan(roman.lastIndexOf('M'));
+        assertThat(roman.indexOf('C')).isGreaterThan(roman.lastIndexOf('M'));
+    }
+
+}
--- src/test/java/fr/arolla/romannumerals/RomanNumeralsShould.java
+++ src/test/java/fr/arolla/romannumerals/RomanNumeralsShould.java
@@ -6,37 +6,12 @@ import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(JUnitParamsRunner.class)
 public class RomanNumeralsShould {
 
-    private Object[] parametersForReturn_roman_with_numeric_value() {
-        return new Object[][] {
-                {1,"I"},
-                {2,"II"},
-                {3,"III"},
-                {4,"IV"},
-                {5,"V"},
-                {6,"VI"},
-                {9,"IX"},
-                {10,"X"},
-                {40,"XL"},
-                {50,"L"},
-                {90,"XC"},
-                {100,"C"},
-                {400,"CD"},
-                {500,"D"},
-                {900,"CM"},
-                {1000,"M"},
-                {1954,"MCMLIV"},
-                {2018,"MMXVIII"},
-        };
-    }
+    private RomanNumerals romanNumerals = new RomanNumeralsWithMap();
 
     @Test
     @Parameters
@@ -44,28 +19,15 @@ public class RomanNumeralsShould {
         assertThat(toRoman(numericValue)).isEqualTo(romanValue);
     }
 
-    private String toRoman(int numericValue) {
-        return repeat("I", numericValue)
-                .replaceAll("I{1000}","M")
-                .replaceAll("I{900}","CM")
-                .replaceAll("I{500}","D")
-                .replaceAll("I{400}","CD")
-                .replaceAll("I{100}","C")
-                .replaceAll("I{90}","XC")
-                .replaceAll("I{50}","L")
-                .replaceAll("I{40}","XL")
-                .replaceAll("I{10}","X")
-                .replaceAll("I{9}","IX")
-                .replaceAll("I{5}","V")
-                .replaceAll("I{4}","IV");
+    private Object[] parametersForReturn_roman_with_numeric_value() {
+        return new Object[][]{
+                {1, "I"}, {2, "II"}, {3, "III"}, {4, "IV"}, {5, "V"},
+                {6, "VI"}, {9, "IX"}, {10, "X"}, {40, "XL"}, {50, "L"},
+                {90, "XC"}, {100, "C"}, {400, "CD"}, {500, "D"}, {900, "CM"},
+                {1000, "M"}, {4999, "MMMMCMXCIX"}, {2018, "MMXVIII"}};
     }
 
-    private String repeat(String letter, int numericValue) {
-        String result = "";
-        for (int i = 0; i < numericValue; i++) {
-            result += letter;
-        }
-
-        return result;
+    private String toRoman(int numericValue) {
+        return romanNumerals.toRoman(numericValue);
     }
 }
